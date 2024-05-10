cee471606d896862f878034df208494d4c77c625
Mario Climent Jiménez <mclijim@epsg.upv.es>
2024-05-11 00:32:05 +0200

    gitflow-feature-stash: H4.3-14

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,59 @@ import Register from './register';
 import Login from './login';
 import VideoSection from './video_section'
 
+import {BrowserRouter, Routes, Route, Navigation} from "react-router-dom"
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoutes';
+
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login"/>
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
+
+/*
 function App() {
   return (
     <div className="App">
       <Header /> 
       <main className='main'>
-        {/* <Component /> */}
+        {/* <Component /> *//*}
         <Register />
       </main>
       <Footer />
     </div>
   );
 }
+*/
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                  <Home />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+
+        
+      </BrowserRouter>
+    </>
+  );
+}
+
 
 export default App;
---
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,26 @@
+import {Navigate} from "react-router-dom"
+import {jwtDecode} from "jwt-decode"
+import api from "../api"
+import { REFRESH_TOKEN, ACCES_TOKEN } from "../constants"
+import { useState } from "react"
+
+function ProtectedRoute({children}){
+    const [isAuthorized, setIsAuthorized] = useState(null)
+
+    /*1:03:08*/
+    const refreshToken = async() => {
+
+    }
+
+    const auth = async() => {
+        
+    }
+
+    if (isAuthorized === null){
+        return <div>Loading...</div>
+    }
+
+    return isAuthorized ? children : <Navigate to="/login"/>
+}
+
+export default ProtectedRoute
