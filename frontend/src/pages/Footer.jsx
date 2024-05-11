986d468071e5e748136e48713dbd3a5c57efa957
Jorge Satorres <jorgesatorres007@gmail.com>
2024-05-12 00:26:34 +0200

    Login, register, admin, user, header y footer responsive
    Todas las paginas antes mencionadas ya estan subidas al servidor y son responsive

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../styles/Footer.css';
-import logo from '../multimedia/logo.svg'; // Importa la imagen de React
+import logo from '../multimedia/logoDadumo.png'; // Importa la imagen de React
 
 function Footer() {
   return (
     <footer className="App-footer">
       <img src={logo} className="Logo-footer" alt="logo" />
       <div className="footer-buttons">
-        <button>Solución</button>
-        <button>Registro</button>
-        <button>Inicio</button>
-        <button className='btn-final'>Contáctanos</button>
+        <a>Solución</a>
+        <a href='./register'>Registro</a>
+        <a>Inicio</a>
+        <a href='./contacto' className='btn-final'>Contáctanos</a>
       </div>
     </footer>
   );
--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import '../styles/Header.css';
-import logo from '../multimedia/logo.svg'; // Importa la imagen de React
+import logo from '../multimedia/logoDadumo.png';
+import { FaBars, FaTimes } from "react-icons/fa"
+import { useRef } from "react"
 
 function Header() {
+
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <div className="header-buttons">
-        <button className='btn-second'>Solución</button>
-        <button className='btn-second'>Registro</button>
-        <button className='btn-second'>Inicio</button>
-        <button className='btn-main'>Contáctanos</button>
-      </div>
+    <header>
+      <img src={logo} className="logo-header" alt="logo" />
+      <nav ref={navRef}>
+        <a>Solución</a>
+        <a href='./register'>Registro</a>
+        <a>Inicio</a>
+        <a href='./contacto'>Contáctanos</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
     </header>
   );
 }
