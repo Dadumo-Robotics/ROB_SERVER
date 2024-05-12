f910ca134288492f12bb0938c28fde984d0d5ca1
Rekken4848 <hugomarescrihuela@gmail.com>
2024-05-12 21:00:07 +0200

    Implementacion Pagina Movimiento en Srv

--- frontend/src/pages/Movement.jsx
+++ frontend/src/pages/Movement.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import '../styles/Movement.css';
 import '../App.css';
-import movement from '../movement';
+import camara_foto from '../multimedia/FotosMovimiento/camara_casera.png';
+import mapa_foto from '../multimedia/FotosMovimiento/mapa.png';
+//import movement from '../movement';
 
 function Movement() {
   return (
-    
+
     <div className="app-movement">
       <main className='main-movement'>
         <div className='movement-container'>
-            <div id="camera">
-                <img src="camara-imagen.jpg" alt="Vista de la cámara" />
-                <div id="controls">
-                    <button id="back-button">Volver</button>
-                    <button id="other-button">Otra Acción</button>
+          <div id="camera">
+            <img src={camara_foto} alt="Vista de la cámara" />
+              <div id="controls">
+                <button id="back-button">Volver</button>
+                <button id="other-button">Trayecto</button>
+              </div>
+              <div id="map-container">
+                <div id="map">
+                  <img src={mapa_foto} alt="Minimapa" />
                 </div>
-                <div id="map">Minimapa</div>
-                <div id="arrows">
-                    <div class="arrow-box" id="vertical-arrows">
-                        <button class="arrow">↑</button>
-                        <button class="arrow">↓</button>
-                    </div>
-                    <div class="arrow-box" id="horizontal-arrows">
-                        <button class="arrow">←</button>
-                        <button class="arrow">→</button>
-                    </div>
+                <div id="coordinates">Coordenadas: X, Y</div>
+              </div>
+              <div id="arrows">
+                <div className="arrow-box" id="vertical-arrows">
+                  <button className="arrow" id="btn_move_up">↑</button>
+                  <button className="arrow" id="btn_move_down">↓</button>
                 </div>
-            </div>
+                <div className="arrow-box" id="horizontal-arrows">
+                  <button className="arrow" id="btn_move_left">←</button>
+                  <button className="arrow" id="btn_move_right">→</button>
+                </div>
+              </div>
+          </div>
         </div>
       </main>
     </div>
