04e51226b7b00ee88357a6e9f6e7b67425a318b7
Mario Climent Jiménez <mclijim@epsg.upv.es>
2024-05-29 01:52:43 +0200

    Actualización de las vistas de usuario
    Añadimos las vistas de Update, Retrieve y List

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,8 @@
+# users/views.py
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from django.http import JsonResponse
-from django.core.mail import send_mail
 from .models import *
 
 # Create your views here.
@@ -17,3 +15,18 @@ class CreateUserView(generics.CreateAPIView):
 class DeleteUserView(generics.DestroyAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [AllowAny]
+
+class UpdateUserView(generics.UpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class RetrieveUserView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class ListUsersView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
