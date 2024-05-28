637a69b466f8a9f037fdb8b25f203f1a0d117f64
Mario Climent Jiménez <mclijim@epsg.upv.es>
2024-05-29 01:53:33 +0200

    Nombre al inicio del archivo

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,4 @@
+# users/urls.py
 from django.urls import path
 from . import views
 
@@ -5,7 +6,3 @@ urlpatterns = [
     path("user/register/", views.CreateUserView.as_view(), name="register"),
     path("user/delete/<int:pk>", views.DeleteUserView.as_view(), name="delete"),  
 ]
-
-
-
-
