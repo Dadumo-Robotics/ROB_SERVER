57139ca5a842e9af48aeed63f589bdd6c3eb0661
Mario Climent Jiménez <mclijim@epsg.upv.es>
2024-05-29 11:23:01 +0200

    Añadiddos valores a consants.js
    Valores:
    -ROBOT_ID
    -ROBOT_IP
    -ROBOT

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,5 @@
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh"
+export const ROBOT_ID = "robot_id";
+export const ROBOT_IP = "robot_ip";
+export const ROBOT = "robot";
